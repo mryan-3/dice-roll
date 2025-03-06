54e0e28025f55c7ae82c94b33c372fe642a0568a
mryan-3 <malawaryan@gmail.com>
2025-03-06 11:56:37 +0300

    update

--- main.py
+++ main.py
@@ -7,9 +7,20 @@ total_rolls = 1000
 for _ in range(total_rolls):
     rand = random.random()
 
-    face = int(rand * 6) + 1
+    if rand < 1/6:
+        face_counts[1] += 1
+    elif rand < 2/6:
+        face_counts[2] += 1
+    elif rand < 3/6:
+        face_counts[3] += 1
+    elif rand < 4/6:
+        face_counts[4] += 1
+    elif rand < 5/6:
+        face_counts[5] += 1
+    else:
+        face_counts[6] += 1
 
-    face_counts[face] += 1
+total_frequency = 0
 
 print("Face\tFrequency\tPercentage")
 print("-" * 35)
@@ -17,5 +28,10 @@ print("-" * 35)
 # Print results for each face
 for face in range(1, 7):
     frequency = face_counts[face]
+    total_frequency += frequency
+
     percentage = (frequency / total_rolls) * 100
-    print(f"{face}\t{frequency}\t\t{percentage:.2f}%")
+    print(f"{face}\t{frequency}\t\t{percentage:.1f}%")
+
+print("-" * 35)
+print(f"Total frequency: {total_frequency}")
